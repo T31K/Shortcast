0b5e8130d7d4e2ae3e8897b69ea7639440a6a7ab
T31K <t31kmunwong@gmail.com>
2023-02-25 22:24:54 +0800

    autoupdate

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,5 @@
 import { useContext, useState } from 'react';
 import TokenContext from './stores/TokenContext.js';
-
 import App from './App.jsx';
 import Login from './Login.jsx';
 
@@ -8,7 +7,7 @@ function Main() {
   const [tokens, setTokens] = useState({
     id: 1,
     access_token:
-      'BQBNi5vQnpJNljZwMOSw69PQvTdbGRrTx9t8OFg7Nz0Hp5FVEyB9d6sdhKsvYJrC2y7ZgB0QJSUkGm5KG0tXwtMF62H08MaPia_6uluU9v_Nl_J3NqPxVhB0EV4aroR4RuoNZQDmmOAetxmA5VTuMZrjF7PTOzBGdusS8Y__zpATBvjra9rZ5vc5OUd_95oLDXT5AHqw-Mh0zv4fRv_vFFC26YBFAYwT9h4',
+      'BQD3WIlP-8DRLX1x3TAAdISHcymP_mowdGe1zws5mtyzSSi5Ac2nhpCrIIQXUY0TkKb93_FlhhrfsY35-m380fGleeWtCyUlyKB3avDtgfELc2oVQFQnvJ0AWcdLiEdonANwXncWViU0Jc0KdHIMT4Qx4ITwnDcZucor6ZCPhBgy3Gd9yDcxDGMlCaAOLU5CkA6ZtScjADEekC3psIirsqa6h816DgOBL48',
     token_type: 'Bearer',
     expires_in: 3600,
     refresh_token:
@@ -20,13 +19,7 @@ function Main() {
   return (
     <TokenContext.Provider value={{ tokens, setTokens }}>
       {/* {JSON.stringify(tokens.access_token)} */}
-      {Object.keys(tokens).length === 0 ? (
-        <Login />
-      ) : (
-        <>
-          <App />
-        </>
-      )}
+      <main className="dialog">{Object.keys(tokens).length !== 0 ? <Login /> : <App />}</main>
     </TokenContext.Provider>
   );
 }
